2ee149d2a9cd3ad590b356fa5618a6f0ef794858
Aaron Bukowski <abukowski21@gmail.com>
2024-09-25 11:04:20 -0400

    [sty]: rename examples, add more code to input.

---
+++ example/in.f90
@@ -0,0 +1,33 @@
+program demo
+    integer :: endif,if,elseif
+    integer,DIMENSION(2) :: function
+    endif=3;if=2
+    if(endif==2)then
+    endif=5
+    elseif=if+4*(endif+&
+    2**10)
+    elseif(endif==3)then
+    function(if)=endif/elseif
+    print*,endif
+    write (*,*)"something"
+    endif
+
+endIF
+endDO
+endSELECT
+endASSOCIATE
+endBLOCK
+endSUBROUTINE
+endFUNCTION
+endMODULE
+endSUBMODULE
+endTYPE
+endPROGRAM
+endINTERFACE
+endENUM
+endWHERE
+endFORALL
+
+
+
+    end progra
